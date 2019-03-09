33ee6b10ada41d8e1239e2dc2d0862fa74961d2c
Cian <cianharry@gmail.com>
2019-03-09 11:24:54 +0000

    added ContractHUB firebase credentials to the app.js var config

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var firebase = require("firebase");
 import firebase from "firebase";
+
 // Initialize Firebase
-// TODO: Replace with your project's customized code snippet
 var config = {
- ",
+    apiKey: "AIzaSyD5iKSY-85eQiHvymdAPHULQM6iX9HSFQ0",
+    authDomain: "contracthub-caa72.firebaseapp.com",
+    databaseURL: "https://contracthub-caa72.firebaseio.com",
+    projectId: "contracthub-caa72",
+    storageBucket: "contracthub-caa72.appspot.com",
+    messagingSenderId: "408056838234"
   };
   firebase.initializeApp(config);
