98914c091c49530010ebaae09ee57f2575f1da03
Cian Harrison <cianharry@gmail.com>
2019-03-22 16:35:50 +0000

    Updated databse connection and incorporated handlebars to display DB data

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+const functions = require('firebase-functions');
+const express = require('express');
+
+const app = express();
+app.get('/timestamp-cached', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    response.send("${Date.now()}");
+});
+
+app.get('/timestamp', (request, response) => {
+    response.send("${Date.now()}");
+});
+
+exports.app = functions.https.onRequest(app);
+app.get('/timestamp', (request, response) => {
+    response.send("${Date.now()}");
+});
